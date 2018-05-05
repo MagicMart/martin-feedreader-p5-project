0241f3c3973e537f852509dd55a2b742f37e3d65
Martin Tudor <martcan27@gmail.com>
2018-05-05 12:04:39 +0100

    Add new tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -38,7 +38,7 @@ $(function() {
                 expect(url).not.toBe(undefined);
                 expect(url).not.toBe('');
 
-            })
+            });
         });
 
 
@@ -52,7 +52,7 @@ $(function() {
                 expect(name).not.toBe(undefined);
                 expect(name).not.toBe('');
 
-            })
+            });
         });
 
 
@@ -62,17 +62,36 @@ $(function() {
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
+         */
 
+        it('has menu element hidden by default', function() {
+            const menu = document.body.getAttribute('class');
+            console.log(menu);
+            expect(menu).toBe('menu-hidden');
+        })
     /* TODO: Write a test that ensures the menu changes
      * visibility when the menu icon is clicked. This test
      * should have two expectations: does the menu display when
      * clicked and does it hide when clicked again.
      */
+
+
+
+     it('toggles the menu display when clicked', function() {
+          const menu = document.querySelector('a');
+          const body = document.querySelector('body');
+          menu.click();
+          expect(body.className).toBe('');
+          menu.click();
+          expect(body.className).toBe('menu-hidden');
+     });
+
+
+      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -89,5 +108,5 @@ $(function() {
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
- });
+
 }());
